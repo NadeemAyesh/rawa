ce57875b3c7bf794498a1cb18e318ff54d416ab7
n.ayesh <n.ayesh@newline.ps>
2020-09-13 17:07:45 +0300

    update products

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 
+// Y axis scroll speed
+var velocity = 0.1;
+
+function update() {
+  var pos = $(window).scrollTop();
+  $('.bg-after').each(function () {
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height() - 18;
+    $(this).css('backgroundPosition', 'right ' + Math.round((height - pos) * velocity) + 'px');
+  });
+};
+
+$(window).bind('scroll', update);
 
 (function ($) {
 
